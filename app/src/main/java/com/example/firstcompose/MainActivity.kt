e4964ef0cb5683e31570cf18655f48df355bb134
Mohit Goel <itsmohitgoel@gmail.com>
2023-11-04 06:22:39 +0000

    LaunchedEffect example 2

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -4,50 +4,47 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import java.lang.Exception
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Counter()
+            LaunchEffectComposable()
         }
     }
 }
 
 @Preview(showSystemUi = true)
 @Composable
-fun Counter() {
-    var count = remember { mutableStateOf(0) }
-    var isDivisibleByThree = (count.value % 3 == 0)
-    LaunchedEffect(key1 = isDivisibleByThree) {
-        Log.d("Counter", "Current count : ${count.value} | keyValue  = $isDivisibleByThree")
+fun LaunchEffectComposable() {
+    val counter = remember { mutableStateOf(0) }
+    var scope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = Unit) {
+        Log.d("LaunchEffectComposable", "Started..")
+        try {
+            for (i in 1..10) {
+                counter.value++
+                delay(1000)
+            }
+        } catch (e: Exception) {
+            Log.d("LaunchEffectComposable", e.message.toString())
+        }
     }
-    Button(onClick = {
-        count.value++
-    }) {
-        Text(text = "Increment Count")
+
+    var text = "Counter is Running | ${counter.value}"
+    if (counter.value == 10) {
+        text = "Counter Stopped"
     }
+    Text(text = text)
 }

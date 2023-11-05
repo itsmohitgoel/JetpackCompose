56da7b1473d309b67d0d55d72fb4db456a2ea542
Mohit Goel <itsmohitgoel@gmail.com>
2023-11-05 07:42:53 +0000

    :construction: ProduceState Ex2

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -1,27 +1,15 @@
 package com.example.firstcompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.ProduceStateScope
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.firstcompose._11_produce_state.ProduceStateComp
-import kotlinx.coroutines.delay
-import java.lang.Exception
+import com.example.firstcompose._11_produce_state.ProduceStateComp2
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProduceStateComp()
+            ProduceStateComp2()
         }
     }
 }

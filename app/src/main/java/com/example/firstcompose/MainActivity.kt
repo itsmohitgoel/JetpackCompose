311890fc128e0b804fe62429fccfa2922e8eebef
Mohit Goel <itsmohitgoel@gmail.com>
2023-11-05 07:31:12 +0000

    ProduceState ex

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.ProduceStateScope
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.firstcompose._11_produce_state.ProduceStateComp
 import kotlinx.coroutines.delay
 import java.lang.Exception
 
@@ -19,32 +21,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LaunchEffectComposable()
+            ProduceStateComp()
         }
     }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun LaunchEffectComposable() {
-    val counter = remember { mutableStateOf(0) }
-    var scope = rememberCoroutineScope()
-
-    LaunchedEffect(key1 = Unit) {
-        Log.d("LaunchEffectComposable", "Started..")
-        try {
-            for (i in 1..10) {
-                counter.value++
-                delay(1000)
-            }
-        } catch (e: Exception) {
-            Log.d("LaunchEffectComposable", e.message.toString())
-        }
-    }
-
-    var text = "Counter is Running | ${counter.value}"
-    if (counter.value == 10) {
-        text = "Counter Stopped"
-    }
-    Text(text = text)
 }

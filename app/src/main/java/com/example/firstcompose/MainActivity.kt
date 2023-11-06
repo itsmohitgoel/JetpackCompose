39dcb7ea18f5911ead425cd04d785ffcb5ae4df0
Mohit Goel <itsmohitgoel@gmail.com>
2023-11-06 07:18:03 +0000

    :construction: DerivedState Ex1

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.firstcompose._11_produce_state.ProduceStateComp2
+import com.example.firstcompose._12_derived_state.DerivedStateComp1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProduceStateComp2()
+            DerivedStateComp1()
         }
     }
 }

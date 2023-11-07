2469b459fc99af6e8320c0aa6e8e925ae5e67e4c
Mohit Goel <itsmohitgoel@gmail.com>
2023-11-07 17:42:27 +0000

    :construction: Add Retrofit interface with api calls

---
+++ app/src/main/java/com/example/firstcompose/remote/NetworkService.kt
@@ -0,0 +1,18 @@
+package com.example.firstcompose.remote
+
+import com.example.firstcompose.models.TweetDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+
+interface NetworkService {
+
+    @GET("/v3/b/660383332b1b334a633bf063?meta=false")
+    suspend fun getTweets(@Header("X-JSON-Path") category: String): Response<List<TweetDto>>
+
+
+    @GET("/v3/b/660383332b1b334a633bf063?meta=false")
+    @Headers("X-JSON-Path:tweets..category")
+    suspend fun getCategories(): Response<Set<String>>
+}

edf9fa0d7897d5d4630738a297f8516447a35a60
Mohit Goel <itsmohitgoel@gmail.com>
2023-11-07 10:13:22 +0000

    :construction: Initial Project Setup for Basic compose app

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -3,14 +3,13 @@ package com.example.firstcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.firstcompose._11_produce_state.ProduceStateComp2
-import com.example.firstcompose._12_derived_state.DerivedStateComp1
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DerivedStateComp1()
+            Text(text = "Hello World")
         }
     }
 }

8d96add1221dcbe599977464423fe56fe6c9173d
Mohit Goel <itsmohitgoel@gmail.com>
2023-11-09 09:12:33 +0000

    :construction: Create Repository to deal with possibly multiple datasources.

---
+++ app/src/main/java/com/example/firstcompose/repository/TweetRepository.kt
@@ -0,0 +1,33 @@
+package com.example.firstcompose.repository
+
+import com.example.firstcompose.models.TweetDto
+import com.example.firstcompose.remote.NetworkService
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import retrofit2.Response
+import javax.inject.Inject
+
+class TweetRepository @Inject constructor(
+    private val networkService: NetworkService
+) {
+    private val _categories = MutableStateFlow<Set<String>>(emptySet())
+    val categories: StateFlow<Set<String>>
+        get() = _categories
+
+    private val _tweets = MutableStateFlow<List<TweetDto>>(emptyList())
+    val tweets: StateFlow<List<TweetDto>>
+        get() = _tweets
+
+    suspend fun getCategories() {
+        val response: Response<Set<String>> = networkService.getCategories()
+        if (response.isSuccessful && response.body() != null) {
+            _categories.emit(response.body()!!)
+        }
+    }
+
+    suspend fun getTweets(category: String) {
+        val response = networkService.getTweets(category)
+        if (response.isSuccessful && response.body() != null)
+            _tweets.emit(response.body()!!)
+    }
+}

85ffcec686cac185a738f65d4a98cafd41b7e418
Mohit Goel <itsmohitgoel@gmail.com>
2023-11-14 14:33:41 +0000

    :construction: DetailScreen display API data.

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -1,30 +1,19 @@
 package com.example.firstcompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import com.example.firstcompose.remote.NetworkService
+import com.example.firstcompose.screens.DetailScreen
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var networkService: NetworkService
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        GlobalScope.launch {
-            var response = networkService.getCategories()
-            Log.d("MOGO", response.body().toString())
-        }
         setContent {
-            Text(text = "Hello World")
+            DetailScreen()
         }
     }
 }
+
--- app/src/main/java/com/example/firstcompose/viewmodels/DetailViewModel.kt
+++ app/src/main/java/com/example/firstcompose/viewmodels/DetailViewModel.kt
@@ -12,7 +12,7 @@ import javax.inject.Inject
 @HiltViewModel
 class DetailViewModel @Inject constructor(val repository: TweetRepository) : ViewModel() {
 
-    val categories: StateFlow<List<TweetDto>>
+    val tweets: StateFlow<List<TweetDto>>
         get() = repository.tweets
 
     init {

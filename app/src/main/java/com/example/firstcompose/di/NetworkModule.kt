60c0ca5647e4243c0e9ced6c63ca1b41f2a8310e
Mohit Goel <itsmohitgoel@gmail.com>
2023-11-15 12:19:24 +0000

    :construction: Define OkHttpClient and HttpLoggingInterceptor.

--- app/src/main/java/com/example/firstcompose/di/NetworkModule.kt
+++ app/src/main/java/com/example/firstcompose/di/NetworkModule.kt
@@ -1,12 +1,16 @@
 package com.example.firstcompose.di
 
+import android.media.TimedMetaData
 import com.example.firstcompose.remote.NetworkService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -15,10 +19,32 @@ class NetworkModule {
 
     @Singleton
     @Provides
-    fun providesRetrofit(): Retrofit {
-        return Retrofit.Builder().baseUrl("https://api.jsonbin.io")
+    fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.jsonbin.io")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addNetworkInterceptor(loggingInterceptor)
+            .readTimeout(2000, TimeUnit.MILLISECONDS)
+            .writeTimeout(2000, TimeUnit.MILLISECONDS)
+            .callTimeout(2000, TimeUnit.MILLISECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.HEADERS)
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
     }
 
     @Singleton
